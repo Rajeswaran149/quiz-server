3298d0ec3517a1de09c54a5ab05942e822afe757
Rajeswaran <rajeswaran149@gmail.com>
2024-10-11 12:12:48 +0530

    connected mongodb

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose")
 
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json()); 
+
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser:true, useUnifiedTopology:true })
+.then(()=> console.log("MongoDB connected"))
+.catch((error) => console.log("MongoDB connection error :", error));
 
 app.use("/",(req,res) => {
     res.send("server is running successfull!!..")
